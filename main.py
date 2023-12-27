b3896e106eeab5a63d4aac338d004f94e93de8e3
BryantBudiono <dabmasterstream@gmail.com>
2023-12-27 20:37:35 +0700

    tambah sprite dan class gamesprite

--- main.py
+++ main.py
@@ -7,6 +7,28 @@ win_height = 500
 window = display.set_mode((win_width, win_height)) 
 window.fill(background)
 #---------
+class GameSprite(sprite.Sprite):
+    def __init__ (self, player_image, player_x, player_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (65,65))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def moveupdate(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 1:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 620:
+            self.rect.x += self.speed
+    def fire(self):
+        bullet = Bullet("bullet.png", self.rect.centerx, self.rect.top, -15)
+        bullets.add(bullet)
+#---------
 clock = time.Clock()
 FPS = 60
 playing = True #game is running
